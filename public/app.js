ab9091884029608023c79a4b22c5f0b55cededdd
wszczawinski <w.k.szczawinski@gmail.com>
2020-02-22 22:07:27 +0100

    Displaying data from DB + creating new todos and sending to DB

--- public/app.js
+++ public/app.js
@@ -2,11 +2,34 @@ $(document).ready( () => {
     $.getJSON('/api/todos')
         .then( addTodos )
         .catch( err => console.log(err));
+    
+    $('#todoInput').keypress( (e) => {
+        if(e.which == 13) {
+            createTodo();
+        };
+    });
 });
 
-function addTodos(todos) {
+const addTodos = (todos) => {
     todos.forEach( todo => {
-        let newTodo = $(`<li class='task'>${todo.name}</li>`);
-        $('.list').append(newTodo);
+        addTodo(todo);
     });
+};
+
+const addTodo = (todo) => {
+    let newTodo = $(`<li class='task'>${todo.name}</li>`);
+        if(todo.completed){
+            newTodo.addClass('done');
+        };
+        $('.list').append(newTodo);
+};
+
+const createTodo = () => {
+    let usrInput = $('#todoInput').val();
+    $.post('/api/todos', {name: usrInput})
+        .then( newTodo => {
+            $('#todoInput').val('')
+            addTodo(newTodo)
+        })
+        .catch( err => console.log(err));
 };
