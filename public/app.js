9ad0f3c5fc552fceb63cf18861c6c1d196d93347
wszczawinski <w.k.szczawinski@gmail.com>
2020-02-24 22:27:51 +0100

    Finishing todo list with Update foncionality

--- public/app.js
+++ public/app.js
@@ -9,36 +9,15 @@ $(document).ready( () => {
         };
     });
 
-    $('.list').on('click', 'span', function() {
+    $('.list').on('click', 'li', function(){
+        updateTodo($(this));
+    });
+
+    $('.list').on('click', 'span', function(e) {
+        e.stopPropagation();
         removeTodo($(this).parent());
     });
 });
-
-const removeTodo = (todo) => {
-    let clickedId =todo.data('id');
-    
-    $.ajax({
-        method: 'DELETE',
-        url: `/api/todos/${clickedId}`
-    })
-        .then(data => todo.remove() )
-        .catch(err => console.log(err));
-}
-
-const addTodos = (todos) => {
-    todos.forEach( todo => {
-        addTodo(todo);
-    });
-};
-
-const addTodo = (todo) => {
-    let newTodo = $(`<li class='task'>${todo.name}<span>X</span></li>`);
-    newTodo.data('id', todo._id);
-        if(todo.completed){
-            newTodo.addClass('done');
-        };
-        $('.list').append(newTodo);
-};
 
 const createTodo = () => {
     let usrInput = $('#todoInput').val();
@@ -49,3 +28,48 @@ const createTodo = () => {
         })
         .catch( err => console.log(err));
 };
+
+const addTodos = (todos) => {
+    todos.forEach( todo => {
+        addTodo(todo);
+    });
+};
+
+const addTodo = (todo) => {
+    let newTodo = $(`<li class='task'>${todo.name}<span>X</span></li>`);
+    newTodo.data('id', todo._id);
+    newTodo.data('completed', todo.completed);
+        if(todo.completed){
+            newTodo.addClass('done');
+        };
+        $('.list').append(newTodo);
+};
+
+const updateTodo = (todo) => {
+    let updateUrl = `/api/todos/${todo.data('id')}`;
+    let isDone = !todo.data('completed');
+    let updatedData = {completed: isDone};
+
+    $.ajax({
+        method: 'PUT',
+        url: updateUrl,
+        data: updatedData
+    })
+        .then(updatedTodo => {
+            todo.toggleClass('done');
+            todo.data('completed', isDone);
+        })
+        .catch(err => console.log(err));
+}
+
+const removeTodo = (todo) => {
+    let deleteUrl = `/api/todos/${clickedId}`;
+    let clickedId =todo.data('id');
+    
+    $.ajax({
+        method: 'DELETE',
+        url: deleteUrl
+    })
+        .then(data => todo.remove() )
+        .catch(err => console.log(err));
+}
